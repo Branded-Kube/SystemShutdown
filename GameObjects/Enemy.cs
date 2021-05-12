550bf6eb8dbb30ad27bec9cc44fd6ab163150099
Frederik <rennowfred@gmail.com>
2021-05-12 17:34:35 +0200

    Threads

--- GameObjects/Enemy.cs
+++ GameObjects/Enemy.cs
@@ -13,116 +13,116 @@ namespace SystemShutdown.GameObjects
 {
     class Enemy /*: GameObject*/
     {
-        //Thread internalThread;
-        //string data;
-        //private bool harvesting = false;
-        //public bool Harvesting
-        //{
-        //    get { return harvesting; }
-        //    set { harvesting = value; }
-        //}
-        //public int id { get; set; }
-        //private Color hoverColor = Color.Gray;
-        //private Color currentColor = Color.White;
-        //private MouseState mouseCurrent;
-        //private MouseState mouseLast;
-        //private Rectangle mouseRectangle;
-        //private Texture2D workersprite;
-        //private string name;
-        //private Rectangle WorkerRectangle;
-        //public event EventHandler ClickSelect;
-        //private Random randomNumber;
-        //private int positionX;
-        //private int positionY;
-        //private Point x;
-        //private Point y;
+        Thread internalThread;
+        string data;
+        private bool harvesting = false;
+        public bool Harvesting
+        {
+            get { return harvesting; }
+            set { harvesting = value; }
+        }
+        public int id { get; set; }
+        private Color hoverColor = Color.Gray;
+        private Color currentColor = Color.White;
+        private MouseState mouseCurrent;
+        private MouseState mouseLast;
+        private Rectangle mouseRectangle;
+        private Texture2D workersprite;
+        private string name;
+        private Rectangle WorkerRectangle;
+        public event EventHandler ClickSelect;
+        private Random randomNumber;
+        private int positionX;
+        private int positionY;
+        private Point x;
+        private Point y;
 
-        //private bool threadRunning = true;
+        private bool threadRunning = true;
 
-        //public bool ThreadRunning
-        //{
-        //    get { return threadRunning; }
-        //    set { threadRunning = value; }
-        //}
-        //public Enemy(string data)
-        //{
-        //    internalThread = new Thread(ThreadMethod);
-        //    randomNumber = new Random();
-        //    positionX = 300 + randomNumber.Next(0, 150);
-        //    positionY = 700 + randomNumber.Next(0, 150);
-        //    x = new Point(positionX, positionY);
-        //    y = new Point(24, 48);
-        //    this.WorkerRectangle = new Rectangle(x, y);
-        //    this.name = data;
+        public bool ThreadRunning
+        {
+            get { return threadRunning; }
+            set { threadRunning = value; }
+        }
+        public Enemy(string data)
+        {
+            internalThread = new Thread(ThreadMethod);
+            randomNumber = new Random();
+            positionX = 300 + randomNumber.Next(0, 150);
+            positionY = 700 + randomNumber.Next(0, 150);
+            x = new Point(positionX, positionY);
+            y = new Point(24, 48);
+            this.WorkerRectangle = new Rectangle(x, y);
+            this.name = data;
 
-        //    //LoadContent(Game1.content);
-        //}
-        //private void LoadContent(ContentManager content)
-        //{
-        //    workersprite = content.Load<Texture2D>("Textures/worker");
-        //}
+            //LoadContent(Game1.content);
+        }
+        private void LoadContent(ContentManager content)
+        {
+            workersprite = content.Load<Texture2D>("Textures/worker");
+        }
 
-        //public void Update()
-        //{
-        //    mouseLast = mouseCurrent;
-        //    mouseCurrent = Mouse.GetState();
-        //    mouseRectangle = new Rectangle(mouseCurrent.X, mouseCurrent.Y, 1, 1);
-        //    if (mouseRectangle.Intersects(WorkerRectangle))
-        //    {
-        //        this.currentColor = hoverColor;
-        //        if (mouseLast.LeftButton == ButtonState.Pressed && mouseCurrent.LeftButton == ButtonState.Released)
-        //        {
-        //            ClickSelect?.Invoke(this, new EventArgs());
-        //        }
-        //    }
-        //    else
-        //    {
-        //        this.currentColor = Color.White;
-        //    }
-        //}
+        public void Update()
+        {
+            mouseLast = mouseCurrent;
+            mouseCurrent = Mouse.GetState();
+            mouseRectangle = new Rectangle(mouseCurrent.X, mouseCurrent.Y, 1, 1);
+            if (mouseRectangle.Intersects(WorkerRectangle))
+            {
+                this.currentColor = hoverColor;
+                if (mouseLast.LeftButton == ButtonState.Pressed && mouseCurrent.LeftButton == ButtonState.Released)
+                {
+                    ClickSelect?.Invoke(this, new EventArgs());
+                }
+            }
+            else
+            {
+                this.currentColor = Color.White;
+            }
+        }
 
-        ///// <summary>
-        ///// Ras - Draws button and its description text in middle of button
-        ///// </summary>
-        //public void Draw(SpriteBatch _spriteBatch)
-        //{
-        //    _spriteBatch.Draw(workersprite, WorkerRectangle, currentColor);
-        //    var x = (WorkerRectangle.X + (WorkerRectangle.Width / 2)) - (GameWorld.font.MeasureString(name).X / 2);
-        //    var y = (WorkerRectangle.Y + (WorkerRectangle.Height / 2)) - (GameWorld.font.MeasureString(name).Y / 2);
-        //    _spriteBatch.DrawString(GameWorld.font, name, new Vector2(x, y), Color.Black);
-        //}
+        /// <summary>
+        /// Ras - Draws button and its description text in middle of button
+        /// </summary>
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            _spriteBatch.Draw(workersprite, WorkerRectangle, currentColor);
+            var x = (WorkerRectangle.X + (WorkerRectangle.Width / 2)) - (GameWorld.font.MeasureString(name).X / 2);
+            var y = (WorkerRectangle.Y + (WorkerRectangle.Height / 2)) - (GameWorld.font.MeasureString(name).Y / 2);
+            _spriteBatch.DrawString(GameWorld.font, name, new Vector2(x, y), Color.Black);
+        }
 
-        ///// <summary>
-        ///// Sets Thread id
-        ///// If any of the 3 bools are true, worker enters corresponding building (Volcano/PalmTree/MainBuilding)
-        ///// </summary>
-        //private void ThreadMethod()
-        //{
-        //    this.id = Thread.CurrentThread.ManagedThreadId;
+        /// <summary>
+        /// Sets Thread id
+        /// If any of the 3 bools are true, worker enters corresponding building (Volcano/PalmTree/MainBuilding)
+        /// </summary>
+        private void ThreadMethod()
+        {
+            this.id = Thread.CurrentThread.ManagedThreadId;
 
-        //    while (GameWorld.running == true)
-        //    {
-        //        if (harvesting == true)
-        //        {
-        //            Debug.WriteLine($"{data}{id} is Running;");
-        //            Thread.Sleep(2000);
+            while (GameWorld.running == true)
+            {
+                if (harvesting == true)
+                {
+                    Debug.WriteLine($"{data}{id} is Running;");
+                    Thread.Sleep(2000);
 
-        //            Debug.WriteLine($"{data}{id} Trying to enter CPU");
+                    Debug.WriteLine($"{data}{id} Trying to enter CPU");
 
-        //            CPU.Enter(internalThread);
+                    CPU.Enter(internalThread);
 
-        //            harvesting = false;
-        //            //delivering = true;
+                    harvesting = false;
+                    //delivering = true;
 
-        //            Debug.WriteLine(string.Format($"{data}{id} shutdown"));
+                    Debug.WriteLine(string.Format($"{data}{id} shutdown"));
 
-        //        }
-        //    }
-        //}
-        //public void Start()
-        //{
-        //    internalThread.IsBackground = true;
-        //    internalThread.Start();
-        //}
+                }
+            }
+        }
+        public void Start()
+        {
+            internalThread.IsBackground = true;
+            internalThread.Start();
+        }
     }
 }
--- GameWorld.cs
+++ GameWorld.cs
@@ -1,8 +1,12 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using SystemShutdown.Buttons;
 using SystemShutdown.GameObjects;
 using SystemShutdown.States;
 
@@ -14,6 +18,22 @@ namespace SystemShutdown
         SpriteBatch spriteBatch;
 
         #region Fields
+
+        public static ContentManager content;
+        public static SpriteFont font;
+        private List<Enemy> enemies;
+        private List<Enemy> delEnemies;
+        private List<Button2> buttons;
+        private Enemy enemy;
+        public static bool running = true;
+        private Button2 spawnEnemyBtn;
+        private Button2 cpuBtn;
+        private Button2 activeThreadsBtn;
+        private Button2 shutdownThreadsBtn;
+        private CPU cpu;
+        private string enemyID = "";
+        private Texture2D cpuTexture;
+        private Texture2D standardBtn;
 
         public static RenderTarget2D renderTarget;
         public float scale = 0.4444f;
@@ -39,6 +59,11 @@ namespace SystemShutdown
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            content = Content;
+            enemies = new List<Enemy>();
+            delEnemies = new List<Enemy>();
+            buttons = new List<Button2>();
         }
         #endregion
 
@@ -54,6 +79,8 @@ namespace SystemShutdown
 
         protected override void Initialize()
         {
+            cpu = new CPU();
+
             /// <summary>
             /// Game runs at 60 fps
             /// Frederik
@@ -80,6 +107,22 @@ namespace SystemShutdown
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = content.Load<SpriteFont>("Fonts/font");
+            standardBtn = content.Load<Texture2D>("Controls/button");
+            cpuTexture = content.Load<Texture2D>("Textures/box");
+            shutdownThreadsBtn = new Button2(800, 840, "Shutdown Threads", standardBtn);
+            activeThreadsBtn = new Button2(1000, 840, "Thread info", standardBtn);
+            spawnEnemyBtn = new Button2(500, 660, "Spawn Enemy", standardBtn);
+            cpuBtn = new Button2(700, 700, "CPU", cpuTexture);
+
+            spawnEnemyBtn.Click += SpawnEnemyBtn_Clicked;
+            shutdownThreadsBtn.Click += ShutdownBtn_Clicked;
+            activeThreadsBtn.Click += ActiveThreadsBtn_Clicked;
+            buttons.Add(shutdownThreadsBtn);
+            buttons.Add(activeThreadsBtn);
+            buttons.Add(cpuBtn);
+
 
             //Loads all GameStates
             //Frederik
@@ -124,6 +167,36 @@ namespace SystemShutdown
                 isGameState = false;
             }
 
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                ShutdownThreads();
+                Exit();
+            }
+
+            foreach (var button in buttons)
+            {
+                button.Update();
+            }
+
+            if (!enemies.Any())
+            {
+
+            }
+            else
+            {
+                foreach (Enemy enemy in enemies)
+                {
+                    if (enemy.ThreadRunning == false)
+                    {
+                        enemies.Remove(enemy);
+                    }
+                }
+                foreach (Enemy enemy in enemies)
+                {
+                    enemy.Update();
+                }
+            }
+
             base.Update(gameTime);
         }
 
@@ -158,6 +231,25 @@ namespace SystemShutdown
                 spriteBatch.Begin();
             }
 
+            foreach (var button in buttons)
+            {
+                button.Draw(spriteBatch);
+            }
+            //Draw selected Enemy ID
+            spriteBatch.DrawString(font, $"Enemy: {enemyID} selected", new Vector2(300, 100), Color.Black);
+
+            if (!enemies.Any())
+            {
+
+            }
+            else
+            {
+                foreach (Enemy enemy in enemies)
+                {
+                    enemy.Draw(spriteBatch);
+                }
+            }
+
 
             spriteBatch.Draw(renderTarget, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
             spriteBatch.End();
@@ -165,6 +257,88 @@ namespace SystemShutdown
             base.Draw(gameTime);
         }
 
+        /// <summary>
+        /// Adds an enemy when button is clicked, and also adds enemy to the other list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SpawnEnemyBtn_Clicked(object sender, EventArgs e)
+        {
+            running = true;
+
+            enemy = new Enemy($"Enemy ");
+            enemy.Start();
+            enemy.ClickSelect += Enemy_ClickSelect;
+            enemies.Add(enemy);
+            delEnemies.Add(enemy);
+        }
+
+        /// <summary>
+        /// Enables clicking on the CPU, and sets enemy ID
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Enemy_ClickSelect(object sender, EventArgs e)
+        {
+            cpuBtn.Click += CPU_Clicked;
+
+            enemy = (Enemy)sender;
+            int ID = enemy.id;
+            Debug.WriteLine(ID);
+            enemyID = ID.ToString();
+        }
+
+        /// <summary>
+        /// Toggles bool on latest clicked enemy and removes click events on CPU
+        /// Enemy thread enters CPU 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CPU_Clicked(object sender, EventArgs e)
+        {
+            enemy.Harvesting = true;
+
+            cpuBtn.Click -= CPU_Clicked;
+        }
+
+        /// <summary>
+        /// Shows all threads aktive and Total number
+        /// Used for debugging purpose only and is not part of the game. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ActiveThreadsBtn_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process procces = System.Diagnostics.Process.GetCurrentProcess();
+            System.Diagnostics.ProcessThreadCollection threadCollection = procces.Threads;
+            string threads = string.Empty;
+            foreach (System.Diagnostics.ProcessThread proccessThread in threadCollection)
+            {
+                threads += string.Format("Thread Id: {0}, ThreadState: {1}\r\n", proccessThread.Id, proccessThread.ThreadState);
+            }
+            Debug.WriteLine($"{threads}");
+            int number = Process.GetCurrentProcess().Threads.Count;
+            Debug.WriteLine($"Total number of aktive threads: {number}");
+        }
+
+        /// <summary>
+        /// Shutdown all enemy threads and clears enemies from draw/update list
+        /// Used both as a button for testing and at game exit
+        /// </summary>
+        public void ShutdownThreads()
+        {
+            running = false;
+            enemies.Clear();
+        }
+        /// <summary>
+        /// Calls ShutdownThreads method on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShutdownBtn_Clicked(object sender, EventArgs e)
+        {
+            ShutdownThreads();
+        }
         #endregion
     }
 }
